396b44e7303496442f122b4ab011622f4383b544
Ben Bolker <bbolker@gmail.com>
2019-03-16 13:54:38 -0400

    update funs for GH loc

--- gapminder_funs.R
+++ gapminder_funs.R
@@ -1,8 +1,8 @@
 require(googlesheets)
 require(dplyr)
-gm_ind <- readr::read_csv("http://ms.mcmaster.ca/bolker/misc/gapminder_index.csv")
-region_tab <- readr::read_csv("http://ms.mcmaster.ca/bolker/misc/country_regions.csv")
-
+loc <- "https://bbolker.github.io/mpha_2019/"
+gm_ind <- readr::read_csv(paste0(loc,"gapminder_index.csv"))
+region_tab <- readr::read_csv(paste0(loc,"country_regions.csv"))
 
 ##' retrieve a gapminder data sheet from Google Docs
 ##' @param target
@@ -15,5 +15,4 @@ get_data <- function(target, multiple_ok=FALSE) {
     }
     return(gs_read(gs_url(ss$URL)))
 }
-if (FALSE) system("scp gapminder_funs.R ms.mcmaster.ca:~/public_html/misc")
 
